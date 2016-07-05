474c30eb610ec6ac7b645c430687dc326756aa06
cmoussa1 <moussa1@llnl.gov>
2016-07-05 14:06:20 -0700

    Added json tags, added exception if file does not have a speed file.

--- net.go
+++ net.go
@@ -2,26 +2,46 @@ package sqrl
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/shirou/gopsutil/net"
 )
 
-type Interfaces struct {
-	Name  string
-	Addrs []net.InterfaceAddr
-	Flags []string
+type Interface struct {
+	Name  string              `json:"name"`
+	Addrs []net.InterfaceAddr `json:"addrs"` // IP Address, MAC Address and their respective ports
+	Flags []string            `json:"flags"` // Types of links (up, down, multicast, etc.)
+	Speed string              `json:"speed"`
 }
 
-func NetworkInfo() {
-	a, _ := net.Interfaces()
-
-	fmt.Println()
-	fmt.Println("\t\t\tNetwork Info")
-
-	for i := 0; i < 5; i++ {
-		info := Interfaces{a[i].Name, a[i].Addrs, a[i].Flags}
-		fmt.Println(info)
+func getInterfaceSpeed(name string) string {
+	path := fmt.Sprintf("/sys/class/net/%s/speed", name)
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "DNE"
 	}
-	fmt.Println()
+	speed := strings.Trim(string(dat), "\n")
+	return speed
+}
 
+func GetInterfaces() ([]Interface, error) {
+	n, err := net.Interfaces()
+	if err != nil {
+		return []Interface{}, err
+	}
+
+	interfaces := []Interface{}
+	for _, i := range n {
+		interfaces = append(
+			interfaces,
+			Interface{
+				Name:  i.Name,
+				Addrs: i.Addrs,
+				Flags: i.Flags,
+				Speed: getInterfaceSpeed(i.Name),
+			},
+		)
+	}
+	return interfaces, nil
 }
