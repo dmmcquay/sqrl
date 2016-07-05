9332fa4b1bed785aa2f828eebc2dca6cea1a1526
Danielle Larson <danielle.r.larson@gmail.com>
2016-07-05 13:17:35 -0700

    I fixed the error issue

--- cmd/sqrld/main.go
+++ cmd/sqrld/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/dmmcquay/sqrl"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/dmmcquay/sqrl"
+)
 
 func main() {
-	sqrl.GetCPUInfo()
+	c, err := sqrl.GetCPUInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	j, err := json.Marshal(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(j))
 }
